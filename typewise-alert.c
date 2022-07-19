e87e37a2dc3408803a493960a69606e6214619a5
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-19 17:30:29 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 struct 
   {
-    int lowerLimit=0;
-    int upperLimit=0;
+    int lowerLimit;
+    int upperLimit;
   } limit;
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
@@ -24,7 +24,7 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit)
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
 {
  
-  struct limit coolinglimit[3] = {[0,35], [0,45], [0,40]};
+  struct limit coolinglimit[3] = {{0,35} {0,45}, {0,40}};
   
   return inferBreach(temperatureInC, coolinglimit[coolingType].lowerLimit, coolinglimit[coolingType].upperLimit);
 
