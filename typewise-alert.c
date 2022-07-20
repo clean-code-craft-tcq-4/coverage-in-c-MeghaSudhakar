c2f5c42b6e9643269f05a5d41a6135023e9370b9
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 12:51:14 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,16 +1,11 @@
-
-
 #include "typewise-alert.h"
 #include <stdio.h>
 #include <string.h>
 #include "inferBreach.h"
-
-
+#include "definition.h"
 
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
 {
-
-
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
 
   switch(alertTarget) 
@@ -28,15 +23,6 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
   }
 }
 
-
-void sendToController(BreachType breachType) 
-{
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
-
-
-
 void checkmail(BreachType breachType) 
 {
   const char* recepient = "a.b@c.com";
@@ -49,11 +35,5 @@ void checkmail(BreachType breachType)
   
 }
 
-void sendToEmail(BreachType breachType) 
-{
-  const char* breachbuffer[3] = {"Hi the temperature is NORMAL\n", "Hi the temperature is TOO_LOW\n", "Hi the temperature is TOO_HIGH\n"}; 
-  printf("%s", breachbuffer[breachType]);  
-  
-}
 
 
