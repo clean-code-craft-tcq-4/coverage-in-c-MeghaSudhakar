f0ecb17d1505a5beaf10f5ab4197ebb27598abd4
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 12:32:00 +0530

    Create inferBreach.h

---
+++ inferBreach.h
@@ -0,0 +1,16 @@
+#include "typewise-alert.h"
+#include <stdio.h>
+#include <string.h>
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
+{
+  if(value <= lowerLimit) 
+  {
+    return TOO_LOW;
+  }
+  if(value >= upperLimit) 
+  {
+    return TOO_HIGH;
+  }
+  return NORMAL;
+}
