7083c80fa24179fce86fffe6e09b4acdb8403732
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 13:34:57 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,8 +1,6 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 #include <string.h>
-#include "inferBreach.h"
-#include "def.h"
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
 {
