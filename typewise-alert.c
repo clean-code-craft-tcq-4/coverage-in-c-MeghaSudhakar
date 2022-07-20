4bf2148b122cd58a9c00136c6adbc36e89df81a1
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 13:34:41 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -4,6 +4,28 @@
 #include "inferBreach.h"
 #include "def.h"
 
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
+{
+  if(value <= lowerLimit) 
+  {
+    return TOO_LOW;
+  }
+  if(value >= upperLimit) 
+  {
+    return TOO_HIGH;
+  }
+  return NORMAL;
+}
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+{
+ 
+  limit_struct_buffer limit_array[3] = {{0,35}, {0,45}, {0,40}};
+  
+  return inferBreach(temperatureInC, limit_array[coolingType].lowerLimit, limit_array[coolingType].upperLimit);
+  
+}
+
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
 {
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
