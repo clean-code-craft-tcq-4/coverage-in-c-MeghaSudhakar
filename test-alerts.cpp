65f7e25bcd23ab46ad9973aae664640e8635eacb
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 15:26:24 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -40,7 +40,7 @@ TEST_CASE("sendToController")
   REQUIRE(sendToController(TOO_HIGH) == NORMAL);
 }
 
-/*TEST_CASE("checkAndAlert") 
+TEST_CASE("func") 
 {
-  REQUIRE(checkAndAlert(TO_CONTROLLER,0,25) == NORMAL);
-}*/
+  REQUIRE(func(TO_CONTROLLER,NORMAL) == NORMAL);
+}
