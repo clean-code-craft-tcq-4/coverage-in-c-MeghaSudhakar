ba6e38c7472f470214f12bf31707cc82dabc1d53
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 12:34:25 +0530

    Update inferBreach.h

--- inferBreach.h
+++ inferBreach.h
@@ -14,3 +14,12 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit)
   }
   return NORMAL;
 }
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+{
+ 
+  limit_struct_buffer limit_array[3] = {{0,35}, {0,45}, {0,40}};
+  
+  return inferBreach(temperatureInC, limit_array[coolingType].lowerLimit, limit_array[coolingType].upperLimit);
+  
+}
