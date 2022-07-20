194e7faac79795ef70fd61ed31e00e5b27fad8ad
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 13:31:29 +0530

    Update inferBreach.c

--- inferBreach.c
+++ inferBreach.c
@@ -4,24 +4,4 @@
 #include "inferBreach.h"
 #include "def.h"
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
-{
-  if(value <= lowerLimit) 
-  {
-    return TOO_LOW;
-  }
-  if(value >= upperLimit) 
-  {
-    return TOO_HIGH;
-  }
-  return NORMAL;
-}
 
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
-{
- 
-  limit_struct_buffer limit_array[3] = {{0,35}, {0,45}, {0,40}};
-  
-  return inferBreach(temperatureInC, limit_array[coolingType].lowerLimit, limit_array[coolingType].upperLimit);
-  
-}
