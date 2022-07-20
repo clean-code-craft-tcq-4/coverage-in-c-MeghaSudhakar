b68d612ee5414b3b555e0dcad19ca3a251e45cd2
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-07-20 13:03:40 +0530

    Update ActionFile.c

--- ActionFile.c
+++ ActionFile.c
@@ -3,15 +3,4 @@
 #include <stdio.h>
 #include <string.h>
 
-void sendToController(BreachType breachType) 
-{
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
 
-void sendToEmail(BreachType breachType) 
-{
-  const char* breachbuffer[3] = {"Hi the temperature is NORMAL\n", "Hi the temperature is TOO_LOW\n", "Hi the temperature is TOO_HIGH\n"}; 
-  printf("%s", breachbuffer[breachType]);  
-  
-}
